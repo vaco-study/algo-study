bbf3a1338bc30e39a0daf1bec42fe97ace094626
junbro <khm11904@gmail.com>
2022-08-01 00:09:54 +0900

    테스트 커밋

---
+++ src/primeNum.js
@@ -0,0 +1,26 @@
+function solution(nums) {
+  let len = nums.length,
+    answer = 0;
+
+  for (let i = 0; i < len - 2; i++) {
+    for (let j = i + 1; j < len - 1; j++) {
+      for (let k = j + 1; k < len; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
+
+const isPrime = (n) => {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
