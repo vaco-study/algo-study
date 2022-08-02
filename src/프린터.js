5df2e9d64ddeb1b4b9b76200091986367a36280b
LeeSyong <jejelsk27@naver.com>
2022-08-02 11:35:43 +0900

    변수명 수정

--- src/프린터.js
+++ src/프린터.js
@@ -1,6 +1,6 @@
 function solution(priorities, location) {
   let mostImportant = Math.max(...priorities);
-  let prinited = 0;
+  let printed = 0;
 
   while(priorities.length > 0) {
     let currDocument = priorities[0];
@@ -10,9 +10,9 @@ function solution(priorities, location) {
     if (currDocument < mostImportant) {
       priorities.push(currDocument);
     } else {
-      prinited++;
+      printed++;
 
-      if (location === 0) return prinited;
+      if (location === 0) return printed;
 
       if (currDocument === mostImportant) {
         mostImportant = Math.max(...priorities);
