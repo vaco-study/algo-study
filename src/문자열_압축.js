acb457340c1c77a92cab0468da4f6b5fdeb6b272
Jinwook Shin <0201jinwook@gmail.com>
2022-08-03 11:29:21 +0900

    feat: [미완료] 제출

--- src/문자열_압축.js
+++ src/문자열_압축.js
@@ -1,42 +1,35 @@
 function solution(s) {
-    if (s.length === 1) {
-        return 1;
-    }
+    if (s.length === 1) return 1;
     
     let answer = 0;
     let unit = 1;
-    const stringLength = s.length;
     const resultContainer = [];
     
-    while (unit <= Math.ceil(stringLength / 2)) {
-        const array = [];
-        
-        for (let i = 0; i < stringLength; i+=unit) {
-            array.push(s.substring(i, i + unit));
-        }
-        
-        let str = "";
-        let temp = array[0];
+    while (unit <= Math.floor(s.length / 2)) {
         let count = 1;
+        let compressedStr = "";
+        
+        for (let i = 0; i < s.length; i+=unit) {
+            const current = s.substr(i, unit);
+            const next = s.substr(i + unit, unit);
 
-        for (let i = 1; i < array.length; i++) {
-            const item = array[i];
-
-            if (temp === item) {
+            if (current === next) {
                 count++;
-            } else {
-                if (count < 2) {
-                    str += temp;
-                } else {
-                    str += `${count}${temp}`;
-                }
-                count = 1;
+                continue;
             }
-            temp = array[i];
+        
+            compressedStr = count > 1 ? compressedStr + count + current : compressedStr + current;
+            count = 1;
         }
+
+        resultContainer.push(compressedStr.length);
         
         unit++;
     }
-        
+    
+    answer = Math.min(...resultContainer);
+    
     return answer;
 }
+
+console.log(solution("aabbacc"));
