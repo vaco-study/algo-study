2e331163d37470cb5ce8eb5892a253bc8a0a5f65
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-08-03 11:34:29 +0900

    fail

--- src/프린터.js
+++ src/프린터.js
@@ -1,17 +1,29 @@
 function solution(priorities, location) {
   let answer = 0;
-  const target = priorities[location];
-  const mostImportantTask = Math.max(...priorities);
+  let printed = 0;
+  let myDocument = location;
 
-  while (mostImportantTask !== priorities[0]) {
-    if (mostImportantTask > priorities[0]) {
-      const shiftedDocuments = priorities.shift();
+  while (priorities.length > 0) {
+    let shiftedDocument = priorities.shift();
 
-      priorities.push(shiftedDocuments);
+    if (
+      priorities.filter((document) => document > shiftedDocument).length > 0
+    ) {
+      priorities.push(shiftedDocument);
+    } else {
+      printed++;
+
+      if (myDocument === 0) {
+        return (answer = printed);
+      }
+    }
+
+    myDocument--;
+
+    if (myDocument === -1) {
+      myDocument = priorities.length - 1;
     }
   }
-
-  answer = priorities.findIndex((element) => target === element) + 1;
-
-  return answer;
 }
+
+console.log(solution([1, 1, 9, 1, 1, 1], 0));
