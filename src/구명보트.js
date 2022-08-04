5c79044561c0b6886d9b2e733616dc6ad7909935
unjinjang <alpsboy1996@gmail.com>
2022-08-04 11:13:35 +0900

    add 구명 보트

---
+++ src/구명보트.js
@@ -0,0 +1,26 @@
+function solution(people, limit) {
+  let left = 0;
+  let right = people.length - 1;
+  let answer = 0;
+
+  people.sort((a, b) => b - a);
+
+  while (left < right) {
+    if (left === right) {
+      answer++;
+
+      break;
+    }
+
+    if (people[left] + people[right] <= limit) {
+      left++;
+      right--;
+    } else {
+      left++;
+    }
+
+    answer++;
+  }
+
+  return answer;
+}
