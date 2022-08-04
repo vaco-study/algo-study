70674252e5f1ad3480fb4df3656ebf02fc83b10e
hansol <stellanew21@gmail.com>
2022-08-04 13:25:46 +0900

    수정

--- src/구명보트.js
+++ src/구명보트.js
@@ -1,16 +1,12 @@
 function solution(people, limit) {
-  const sortedPeople = people.sort((a, b) => a - b);
+  people.sort((a, b) => a - b);
+
   let left = 0;
   let right = people.length - 1;
   let numOfBoats = 0;
 
   while (left <= right) {
-    if (left === right) {
-      numOfBoats++;
-      break;
-    }
-
-    if (sortedPeople[right] + sortedPeople[left] > limit) {
+    if (people[right] + people[left] > limit) {
       right--;
     } else {
       left++;
