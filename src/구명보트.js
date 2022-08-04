e1ed3a6c054c831ab429ffa5871c49af4d80637a
unjinjang <alpsboy1996@gmail.com>
2022-08-04 11:20:46 +0900

    refactor

--- src/구명보트.js
+++ src/구명보트.js
@@ -6,12 +6,6 @@ function solution(people, limit) {
   people.sort((a, b) => b - a);
 
   while (left < right) {
-    if (left === right) {
-      answer++;
-
-      break;
-    }
-
     if (people[left] + people[right] <= limit) {
       left++;
       right--;
