bd6e8eb965d0ce8ba9f4956fdb10b3acc7cc6112
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-08-04 11:19:41 +0900

    test 통과

--- src/모음 사전.js
+++ src/모음 사전.js
@@ -1,10 +1,16 @@
-const collection = ["A", "E", "I", "O", "U"];
+const solution = (word) => {
+  const collection = ["A", "E", "I", "O", "U"];
+
+  const dictionary = getAllPermutations(collection).sort();
+
+  return dictionary.indexOf(word);
+};
 
 const getPermutations = (arr, num) => {
   const results = [];
-  if (num === 1) return arr.map((v) => [v]);
+  if (num === 1) return arr;
 
-  arr.forEach((fixed, index, origin) => {
+  arr.forEach((fixed, _, origin) => {
     const permutations = getPermutations(origin, num - 1);
 
     const attached = permutations.map((v) => [fixed, ...v]);
@@ -12,19 +18,14 @@ const getPermutations = (arr, num) => {
     results.push(...attached);
   });
 
-  return results;
+  return results.map((element) => element.join(""));
 };
 
 const getAllPermutations = (arr) => {
   let results = [];
-  arr.forEach((value, index, origin) => {
+  arr.forEach((_, index, origin) => {
     results.push(...getPermutations(origin, index + 1));
   });
 
   return results;
 };
-
-function solution(word) {
-  let answer = 0;
-  return answer;
-}
