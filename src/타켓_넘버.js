fecbf5b3bfbebb7f85d04c70da4a22861ebbc740
Jinwook Shin <0201jinwook@gmail.com>
2022-08-05 11:16:09 +0900

    feat: 완료 - 제출

---
+++ src/타켓_넘버.js
@@ -0,0 +1,21 @@
+function solution(numbers, target) {
+  const NUMBER_OF_CASES = Math.pow(2, numbers.length);
+  let numberOfWays = 0;
+  
+  for (let i = 0; i < NUMBER_OF_CASES; i++) {
+    const signInBinary = i.toString(2).padStart(4, 0);
+    let equation = ""
+    
+    for (let j = 0; j < numbers.length; j++) {
+      let sign = parseInt(signInBinary[j]);
+      const number = numbers[j];
+      
+      sign ? sign = "+" : sign = "-";
+      equation += sign + number;
+    }
+
+    if (eval(equation) === target) numberOfWays++;
+  }
+      
+  return numberOfWays;
+}
