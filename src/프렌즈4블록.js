b7f951624ef2281205531454f9549a9c6f167690
hansol <stellanew21@gmail.com>
2022-08-08 14:34:12 +0900

    다시 풀기

--- src/프렌즈4블록.js
+++ src/프렌즈4블록.js
@@ -1,36 +1,65 @@
 function solution(m, n, board) {
-  debugger;
-  const totalBlocks = m * n;
-  const comparison = [];
+  const matrix = board.map((column) => column.split(""));
+  const offset = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1],
+  ];
+  let count = 0;
+  let flag = true;
 
-  for (let i = 0; i < m; i++) {
-    const column = [];
-    let left = false;
-    let right = false;
-    let down = false;
+  while (flag) {
+    flag = false;
+    const removedIndex = [];
 
-    for (let j = 0; j < n; j++) {
-      if (board[i][j] === board[i]?.[j - 1]) {
-        left = true;
+    for (let i = 0; i < m - 1; i++) {
+      for (let j = 0; j < n - 1; j++) {
+        const column = [
+          matrix[i][j],
+          matrix[i][j + 1],
+          matrix[i + 1][j],
+          matrix[i + 1][j + 1],
+        ];
+
+        const isSameValue = column.every((value) => value === column[0]);
+
+        if (column[0] && isSameValue) {
+          offset.forEach(([dx, dy]) => {
+            removedIndex.push([dx + i, dy + j]);
+            flag = true;
+          });
+        }
       }
-
-      if (board[i][j] === board[i][j + 1]) {
-        right = true;
-      }
-
-      if (board[i][j] === board[i + 1]?.[j]) {
-        down = true;
-      }
-
-      column.push({ left, right, down });
-
-      left = false;
-      right = false;
-      down = false;
     }
 
-    comparison.push(column);
+    removedIndex.forEach(([i, j]) => {
+      const coordinate = matrix[i][j];
+
+      if (coordinate) {
+        matrix[i][j] = "";
+        count++;
+      }
+    });
+
+    for (let i = 0; i < n; i++) {
+      const stack = [];
+
+      for (let j = 0; j < m; j++) {
+        if (matrix[j][i]) {
+          stack.push(matrix[j][i]);
+        }
+      }
+
+      for (let j = m - 1; j >= 0; j--) {
+        if (stack.length) {
+          matrix[j][i] = stack.pop();
+        } else {
+          matrix[j][i] = "";
+        }
+      }
+    }
   }
 
-  return comparison;
+  return count;
 }
