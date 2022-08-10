0207912244e32f52db4bca90cb1227dee5c0d402
junbro <khm11904@gmail.com>
2022-08-10 11:37:40 +0900

    코드 리팩토링

--- src/큰 수 만들기.js
+++ src/큰 수 만들기.js
@@ -1,22 +1,17 @@
 function solution(number, k) {
   const numArr = number.split("").map((char) => Number(char));
   const stack = [numArr[0]];
-  let deletedCount = 0;
 
   if (k === number.length - 1) return Math.max(...numArr).toString();
 
   for (let i = 1; i < numArr.length; i++) {
-    while (
-      stack.length &&
-      stack[stack.length - 1] < numArr[i] &&
-      deletedCount < k
-    ) {
+    while (stack.length && stack[stack.length - 1] < numArr[i] && k > 0) {
       stack.pop();
 
-      deletedCount++;
+      k--;
     }
 
-    if (k - deletedCount < numArr.length - i) {
+    if (k < numArr.length - i) {
       stack.push(numArr[i]);
     }
   }
