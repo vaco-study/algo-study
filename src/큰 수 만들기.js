362172edd7fb5205e536f4e7c6f9eda117c152d8
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-08-10 11:25:20 +0900

    런타임 에러

---
+++ src/큰 수 만들기.js
@@ -0,0 +1,21 @@
+const solution = (number, k) => {
+  const numberToArray = [...number];
+
+  const combinations = getCombinations(numberToArray, number.length - k);
+  return Math.max(...combinations).toString();
+};
+
+const getCombinations = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((el) => [el]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((el) => fixed + el);
+
+    results.push(...attached);
+  });
+
+  return results;
+};
