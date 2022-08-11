50bd250d35c99710f116db0fbd441d7f1cc4be12
junbro <khm11904@gmail.com>
2022-08-11 11:28:03 +0900

    테스트 케이스 모두 통과 확인 - 각 손님이 주문한 메뉴에서 코스 구성 개수에 해당하는 모든 조합을 구한다. - 특정 조합 코스에 Frequency를 구한다. - 가장 많이 주문한 조합에 대해서 코스요리메뉴에 추가한다. - 마지막에 사전순으로 정렬

---
+++ src/메뉴 리뉴얼.js
@@ -0,0 +1,45 @@
+function solution(orders, course) {
+  const result = [];
+
+  for (const num of course) {
+    const allCases = [];
+    const CaseCounts = {};
+
+    for (const order of orders) {
+      if (num > order.length) continue;
+
+      allCases.push(...getCombinations(order.split(""), num));
+    }
+
+    for (const num of allCases) {
+      CaseCounts[num] = CaseCounts[num] ? CaseCounts[num] + 1 : 1;
+    }
+
+    const maxOrderNum = Math.max(...Object.values(CaseCounts));
+
+    if (maxOrderNum < 2) continue;
+
+    const candidateCases = Object.entries(CaseCounts)
+      .filter((element) => element[1] === maxOrderNum)
+      .map((element) => element[0]);
+
+    result.push(...candidateCases);
+  }
+
+  return result.sort();
+}
+
+const getCombinations = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((el) => [el]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((el) => [fixed, ...el]);
+
+    results.push(...attached);
+  });
+
+  return results.map((element) => element.sort().join(""));
+};
--- src/큰 수 만들기.js
+++ src/큰 수 만들기.js
@@ -1,17 +1,16 @@
 function solution(number, k) {
   const numArr = number.split("").map((char) => Number(char));
-  const stack = [numArr[0]];
+  const stack = [];
+  const targetNum = number.length - k;
 
-  if (k === number.length - 1) return Math.max(...numArr).toString();
-
-  for (let i = 1; i < numArr.length; i++) {
+  for (let i = 0; i < numArr.length; i++) {
     while (stack.length && stack[stack.length - 1] < numArr[i] && k > 0) {
       stack.pop();
 
       k--;
     }
 
-    if (k < numArr.length - i) {
+    if (stack.length < targetNum) {
       stack.push(numArr[i]);
     }
   }
