ff19b2705c6abc826b4bc7959f8f21b1ccd82a6c
hansol <stellanew21@gmail.com>
2022-08-12 12:01:29 +0900

    다시 풀기

--- src/하노이의 탑.js
+++ src/하노이의 탑.js
@@ -1,49 +1,15 @@
 function solution(n) {
-  const first = Array.from({ length: n }, (v, i) => i + 1);
-  const second = [];
-  const third = [];
-  const result = [];
+  return hanoi(n, 1, 2, 3);
+}
 
-  while (third.length !== n) {
-    if (!third.length) {
-      third.unshift(first.shift());
-      result.push([1, 3]);
-    }
-
-    if (first[0] > third[0]) {
-      second.unshift(first.shift());
-      result.push([1, 2]);
-    }
-
-    if (first[0] > second[0] && first[0] > third[0]) {
-      while (third.length) {
-        second.unshift(third.shift());
-        result.push([3, 2]);
-      }
-    }
-
-    if (!first.length) {
-      while (third[0] < second[0]) {
-        third.unshift(second.shift());
-        result.push([2, 3]);
-      }
-
-      while (second.length && second.length !== 1) {
-        first.unshift(second.shift());
-        result.push([2, 1]);
-      }
-    } else {
-      if (!second.length) {
-        third.unshift(first.shift());
-        result.push([1, 3]);
-      }
-
-      while (second.length && second.length !== 1) {
-        first.unshift(second.shift());
-        result.push([2, 1]);
-      }
-    }
+function hanoi(n, start, mid, last) {
+  if (n === 1) {
+    return [[start, last]];
   }
 
-  return result;
+  return [
+    ...hanoi(n - 1, start, last, mid),
+    ...hanoi(1, start, mid, last),
+    ...hanoi(n - 1, mid, start, last),
+  ];
 }
