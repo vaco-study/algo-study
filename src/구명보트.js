92634b0772d57ccf71e78413f7b01b98d1ff8ed4
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-08-15 01:12:21 +0900

    reset commit

--- src/구명보트.js
+++
@@ -1,9 +0,0 @@
-function solution(people, limit) {
-  let answer = 0;
-  const copyPeople = people.slice().sort((a, b) => b - a);
-
-  for (let i = 0, j = copyPeople.length - 1; i <= j; i++, answer++)
-    if (copyPeople[i] + copyPeople[j] <= limit) j--;
-
-  return answer;
-}
