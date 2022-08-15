1ef576b1b384375a83fcb1a32fab0a50f863db4d
junbro <khm11904@gmail.com>
2022-08-15 11:42:01 +0900

    코드 수정

--- src/Task Scheduler.js
+++ src/Task Scheduler.js
@@ -15,19 +15,17 @@ var leastInterval = function (tasks, n) {
       : 1;
   }
 
-  let sortable = Object.entries(frequencyCounter)
-    .sort(([, a], [, b]) => b - a)
-    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+  let sortedCounter = getSortedObj(frequencyCounter);
 
   while (taskFinishedCount < tasks.length) {
     times++;
     let targetKey = null;
 
-    for (const candidatetKey of Object.keys(sortable)) {
+    for (const candidatetKey of Object.keys(sortedCounter)) {
       if (idleCounter[candidatetKey] && idleCounter[candidatetKey] > 0)
         continue;
 
-      if (sortable[candidatetKey] <= 0) continue;
+      if (sortedCounter[candidatetKey] <= 0) continue;
 
       targetKey = candidatetKey;
       break;
@@ -41,19 +39,24 @@ var leastInterval = function (tasks, n) {
       continue;
     }
 
-    sortable[targetKey] -= 1;
+    sortedCounter[targetKey] -= 1;
     for (const key of Object.keys(idleCounter)) {
       idleCounter[key] -= 1;
     }
 
     idleCounter[targetKey] = n;
-
-    sortable = Object.entries(sortable)
-      .sort(([, a], [, b]) => b - a)
-      .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+    sortedCounter = getSortedObj(sortedCounter);
 
     taskFinishedCount++;
   }
 
   return times;
+};
+
+const getSortedObj = (obj) => {
+  const sorted = Object.entries(obj)
+    .sort(([, a], [, b]) => b - a)
+    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+  return sorted;
 };
