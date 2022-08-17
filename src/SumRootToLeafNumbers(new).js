a55d1a7eb4a53768c388fd54c7d6171b261dd23e
Wonil Nam <apotatos11@gmail.com>
2022-08-17 11:24:22 +0900

    리뷰를 위한 파일 수정

---
+++ src/SumRootToLeafNumbers(new).js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function (root) {
+  function makingDataByDFS(input) {
+    const data = [];
+
+    function traverse(node, temp) {
+      const newTemp = temp + node.val;
+      if (node.left) traverse(node.left, newTemp);
+      if (node.right) traverse(node.right, newTemp);
+      if (!node.left && !node.right) {
+        data.push(newTemp);
+      }
+    }
+
+    traverse(input, "");
+    return data;
+  }
+
+  return makingDataByDFS(root).reduce((a, x) => a + Number(x), 0);
+};
