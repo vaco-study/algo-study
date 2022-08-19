a768dbbce9ccda5e6c9fbcd2058d41fc74d5ac4c
Wonil Nam <apotatos11@gmail.com>
2022-08-19 11:02:32 +0900

    Product of Array Except self 풀이 리팩토링

--- src/ProductOfArrayExceptSelf.js
+++ src/ProductOfArrayExceptSelf.js
@@ -2,14 +2,13 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-
 const productExceptSelf = function (nums) {
   let zeroCounter = 0;
-  nums.forEach((element) => {
-    if (element === 0) {
-      zeroCounter++;
-    }
-  });
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) zeroCounter++;
+    if (zeroCounter > 1) break;
+  }
 
   if (zeroCounter > 1) {
     return nums.fill(0);
@@ -24,8 +23,6 @@ const productExceptSelf = function (nums) {
       }
     });
 
-    console.log(totalMulitpleWithoutZero);
-
     return nums.map((element) => {
       if (element === 0) {
         return totalMulitpleWithoutZero;
@@ -33,7 +30,9 @@ const productExceptSelf = function (nums) {
         return 0;
       }
     });
-  } else {
+  }
+
+  if (zeroCounter === 0) {
     const totalMulitple = nums.reduce((a, x) => a * x, 1);
 
     return nums.map((element) => totalMulitple / element);
