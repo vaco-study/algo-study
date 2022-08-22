ee16e00d9650e63fee1478ea90b2b9c7f5dc52aa
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-08-22 11:36:19 +0900

    피로도

---
+++ src/피로도.js
@@ -0,0 +1,9 @@
+const solution = (fatigue, dungeons) => {
+  let clearCount = 0;
+  let dungeonsLength = dungeons.length;
+  const visitedDungeons = newArray(dungeonsLength).fill(false);
+
+  console.log(visitedDungeons);
+
+  return answer;
+};
