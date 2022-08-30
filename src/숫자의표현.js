54988839f01bb6df827750a31e4c87f861cd3952
Wonil Nam <apotatos11@gmail.com>
2022-08-30 10:55:39 +0900

    이해를 위한 한글 변수명 작성

--- src/숫자의표현.js
+++ src/숫자의표현.js
@@ -1,18 +1,16 @@
 function solution(n) {
   let counter = 0;
-
-  const sequenceSum = n;
-  let first;
-  let possibleLength;
-  let lastPossibleLength = n;
+  const 수열의합 = n;
+  let 초항;
+  let 최근연산의마지막항의개수 = n;
 
   for (let i = 1; i <= n; i++) {
-    first = i;
+    초항 = i;
 
-    for (let j = 1; j <= lastPossibleLength; j++) {
-      if (2 * sequenceSum === j * (2 * first + j - 1)) {
+    for (let 항의개수 = 1; 항의개수 <= 최근연산의마지막항의개수; 항의개수++) {
+      if (2 * 수열의합 === 항의개수 * (2 * 초항 + 항의개수 - 1)) {
+        최근연산의마지막항의개수 = 항의개수;
         counter++;
-        lastPossibleLength = j;
         break;
       }
     }
