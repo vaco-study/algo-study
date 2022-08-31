9e95e0a2c0de663eb92c058526c61d21629d24ad
Wonil Nam <apotatos11@gmail.com>
2022-08-31 11:29:12 +0900

    속도향상을 위한 리팩토링 2

--- src/MergeIntervals.js
+++ src/MergeIntervals.js
@@ -2,16 +2,14 @@ const merge = function (intervals) {
   if (intervals.length === 1) return intervals;
 
   intervals.sort((a, b) => a[0] - b[0]);
-
   const result = [intervals[0]];
-  let currentIndex = 0;
 
   for (let i = 1; i < intervals.length; i++) {
     if (result[result.length - 1][1] >= intervals[i][0]) {
-      result[result.length - 1] = [
-        result[result.length - 1][0],
-        Math.max(result[result.length - 1][1], intervals[i][1]),
-      ];
+      result[result.length - 1][1] = Math.max(
+        result[result.length - 1][1],
+        intervals[i][1]
+      );
       continue;
     }
 
