8e9adf2bab5450fd861fcfc6bc8c692321a8a9d4
Wonil Nam <apotatos11@gmail.com>
2022-09-02 10:46:01 +0900

    Count Good Nodes In Binary Tree 문제 풀이

---
+++ src/CountGoodNodesInBinaryTree.js
@@ -0,0 +1,19 @@
+const sumNumbers = function (root) {
+  return makingDataByDFS(root);
+};
+
+const makingDataByDFS = function (root) {
+  let counter = 0;
+
+  const traverse = function (node, maxValue) {
+    if (node.val >= maxValue) {
+      counter++;
+    }
+
+    if (node.left) traverse(node.left, Math.max(maxValue, node.val));
+    if (node.right) traverse(node.right, Math.max(maxValue, node.val));
+  };
+
+  traverse(root, root.val);
+  return counter;
+};
