045d068654428f7fe81d3053169921245522546f
Wonil Nam <apotatos11@gmail.com>
2022-09-06 11:52:23 +0900

    리팩토링 1차

--- src/IntergarToRoman.js
+++ src/IntergarToRoman.js
@@ -4,51 +4,51 @@ const intToRoman = function (num) {
   let result = "";
 
   if (digit >= 4) {
-    result += "M".repeat(Number(numToString[0]));
+    result = result + stringIncoder(4, digit, numToString, []);
   }
 
   if (digit >= 3) {
-    const targetIndex = digit - 3;
-    const targetNumber = Number(numToString[targetIndex]);
-
-    if (targetNumber < 4) {
-      result += "C".repeat(Number(numToString[targetIndex]));
-    } else if (targetNumber === 4) {
-      result += "CD";
-    } else if (targetNumber > 4 && targetNumber < 9) {
-      result += "D" + "C".repeat(Number(numToString[targetIndex]) - 5);
-    } else {
-      result += "CM";
-    }
+    result =
+      result + stringIncoder(3, digit, numToString, ["C", "CD", "D", "CM"]);
   }
 
   if (digit >= 2) {
-    const targetIndex = digit - 2;
-    const targetNumber = Number(numToString[targetIndex]);
-
-    if (targetNumber < 4) {
-      result += "X".repeat(Number(numToString[targetIndex]));
-    } else if (targetNumber === 4) {
-      result += "XL";
-    } else if (targetNumber > 4 && targetNumber < 9) {
-      result += "L" + "X".repeat(Number(numToString[targetIndex]) - 5);
-    } else {
-      result += "XC";
-    }
+    result =
+      result + stringIncoder(2, digit, numToString, ["X", "XL", "L", "XC"]);
   }
 
   if (digit >= 1) {
-    const targetIndex = digit - 1;
+    result =
+      result + stringIncoder(1, digit, numToString, ["I", "IV", "V", "IX"]);
+  }
+
+  return result;
+};
+
+const stringIncoder = function (
+  position,
+  digit,
+  numToString,
+  stringCollection
+) {
+  let result = "";
+
+  if (position >= 4) {
+    result += "M".repeat(Number(numToString[0]));
+  } else {
+    const targetIndex = digit - position;
     const targetNumber = Number(numToString[targetIndex]);
 
     if (targetNumber < 4) {
-      result += "I".repeat(Number(numToString[targetIndex]));
+      result += stringCollection[0].repeat(Number(numToString[targetIndex]));
     } else if (targetNumber === 4) {
-      result += "IV";
+      result += stringCollection[1];
     } else if (targetNumber > 4 && targetNumber < 9) {
-      result += "V" + "I".repeat(Number(numToString[targetIndex]) - 5);
+      result +=
+        stringCollection[2] +
+        stringCollection[0].repeat(Number(numToString[targetIndex]) - 5);
     } else {
-      result += "IX";
+      result += stringCollection[3];
     }
   }
 
